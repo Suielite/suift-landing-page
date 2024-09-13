3daf15da0864488ed179a8f2cdd72e7e9e620a6d
charleschinedum <charleschinedum2@gmail.com>
2024-09-13 18:22:02 +0000

    update: env setup

---
+++ app/utils/constants.ts
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    name: "Home",
+    link: "#home",
+  },
+  {
+    name: "About",
+    link: "#about",
+  },
+  {
+    name: "Features",
+    link: "#features",
+  },
+  {
+    name: "Reviews",
+    link: "#reviews",
+  },
+];
