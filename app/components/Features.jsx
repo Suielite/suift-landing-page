0e8b3327410e368c623939ee769f328c989d7146
Timothy <athanasiuscopy@gmail.com>
2024-09-16 14:07:44 +0100

    first commit

---
+++ app/components/Features.jsx
@@ -0,0 +1,23 @@
+import CommunityPage from "./CommunityPage";
+import "../embla.css";
+
+import CarouselWrapper from "./CarouselWrapper";
+import FaqSection from "./FaqSection";
+import HeaderComponent from "./HeaderComponent";
+
+const Features = () => {
+  return (
+    <div className="mb-9 w-full overflow-hidden">
+      <div className="  w-full overflow-hidden   pb-[100px] relative">
+        <div className="w-full relative">
+          <CarouselWrapper />
+        </div>
+      </div>
+      <div className="px-[120px] flex justify-center  w-full">
+        <CommunityPage />
+      </div>
+    </div>
+  );
+};
+
+export default Features;
